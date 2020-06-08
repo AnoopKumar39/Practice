edb069cd6aa7b06704e6fc8b371d2dc3951380aa
Cloud User <centos@ip-172-31-49-49.ec2.internal>
2020-06-08 18:05:50 +0000

    ss

--- stack.sh
+++ stack.sh
@@ -3,25 +3,31 @@
 LOG=/tmp/logfile
 rm -rf /tmp/logfile
 
+id=$(id -u)
+if [ $id -ne 0 ]; then 
+echo "You should be a root user to perform this action"
+exit 1
+fi
+
 #### Web-Server-Installation ##########
 
 echo "Installing httpd-server"
-yum install httpd -y &>> LOG
+yum install httpd -y &>> $LOG
 echo $?
 
 echo "Updating proxy-config"
 echo 'ProxyPass "/student" "http://APP-SERVER-IPADDRESS:8080/student"
-ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student"' >> /etc/httpd/conf.d/app-proxy.conf &>> LOG
+ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student"' > /etc/httpd/conf.d/app-proxy.conf
 echo $?
 
 echo "Downloading index-file"
-curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/index.html -o /var/www/html/index.html &>> LOG
+curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/index.html -o /var/www/html/index.html &>> $LOG
 echo $?
 
 echo "Enabling-starting httpd"
-systemctl enable httpd &>> LOG
+systemctl enable httpd &>> $LOG
 echo $?
-systemctl start httpd &>> LOG
+systemctl start httpd &>> $LOG
 echo $?
 
 
