3a38b8cae26c1f1d0883df72c72785240e499a47
anoop <anoopkumarnasarapu@gmail.com>
2020-06-09 14:10:29 +0530

    --- Auto-Git Commit ---

--- stack.sh
+++ stack.sh
@@ -3,6 +3,18 @@
 LOG=/tmp/logfile
 rm -rf /tmp/logfile
 
+############ Functions ########
+
+### Status-function #########
+stat() {
+    if [ $? -eq 0 ]; then
+    echo "Sucessful"
+    else
+    echo "Un-sucessfull-please refer log file at the location $LOG"
+}
+
+
+#### Check-whether root user or not ########
 id=$(id -u)
 if [ $id -ne 0 ]; then 
 echo "You should be a root user to perform this action"
