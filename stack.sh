df0181f58961366cbc2c44496d979e852a7522d0
anoop <anoopkumarnasarapu@gmail.com>
2020-06-09 23:52:45 +0530

    ss

--- stack.sh
+++ stack.sh
@@ -18,9 +18,9 @@ N="\e[0m"
 ### Status-function #########
 stat() {
     if [ $? -eq 0 ]; then
-    echo "${G}Sucessful${N}"
+    echo -e "${G}Sucessful${N}"
     else
-    echo "${R}Un-sucessfull-please refer log file at the location $LOG${N}"
+    echo -e "${R}Un-sucessfull-please refer log file at the location $LOG${N}"
     fi
 }
 
@@ -28,26 +28,26 @@ stat() {
 #### Check-whether root user or not ########
 id=$(id -u)
 if [ $id -ne 0 ]; then 
-echo "${R} You should be a root user to perform this action${N}"
+echo -e "${R} You should be a root user to perform this action${N}"
 exit 1
 fi
 
 #### Web-Server-Installation ##########
 
-echo "${B}Installing httpd-server${N}"
+echo -e "${B}Installing httpd-server${N}"
 yum install httpd -y &>> $LOG
 stat $?
 
-echo "${B}Updating proxy-config${N}"
+echo -e "${B}Updating proxy-config${N}"
 echo 'ProxyPass "/student" "http://APP-SERVER-IPADDRESS:8080/student"
 ProxyPassReverse "/student"  "http://APP-SERVER-IPADDRESS:8080/student"' > /etc/httpd/conf.d/app-proxy.conf
 stat $?
 
-echo "${B}Downloading index-file${N}"
+echo -e "${B}Downloading index-file${N}"
 curl -s https://s3-us-west-2.amazonaws.com/studentapi-cit/index.html -o /var/www/html/index.html &>> $LOG
 stat $?
 
-echo "${B}Enabling-starting httpd${N}"
+echo -e "${B}Enabling-starting httpd${N}"
 systemctl enable httpd &>> $LOG
 stat $?
 systemctl start httpd &>> $LOG
